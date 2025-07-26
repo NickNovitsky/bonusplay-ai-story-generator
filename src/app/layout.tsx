90dc807a0bea02ad3768f095a889d21312d09f23
Nick Novitsky <nnovitsky@gamesoft.com>
2025-07-26 15:42:47 +0300

    Minor layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Box, Container, Link, Typography } from "@mui/material";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Container sx={{p: 6}}><Typography sx={{textAlign: "center"}}><Link href="/">BonusPlay</Link></Typography></Container>
         {children}
       </body>
     </html>
