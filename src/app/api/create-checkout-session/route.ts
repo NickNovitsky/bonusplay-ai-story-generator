064925f4dc05470981f8e70416ebd3560e7fdc0f
Nick Novitsky <nnovitsky@gamesoft.com>
2025-08-22 23:04:30 +0300

    feat: init Stripe from lib

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -1,9 +1,6 @@
+import stripe from '@/lib/stripe';
 import { NextRequest, NextResponse } from 'next/server'
-import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil',
-})
 
 export async function POST(req: NextRequest) {
   const { bookId } = await req.json()
--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -1,8 +1,8 @@
 import Stripe from 'stripe'
 import { NextRequest } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
+import stripe from '@/lib/stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2025-04-30.basil' })
 const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
 
 export async function POST(req: NextRequest) {
