2d9b8a4f392add7b67d68e691c6303c5c34a65fb
Nick Novitsky <nnovitsky@gamesoft.com>
2025-08-24 14:35:45 +0300

    feat: restyle/refactor book summary

--- src/components/BookSummary.tsx
+++ src/components/BookSummary.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Typography, Card, CardMedia, Box, Button } from '@mui/material';
+import { Typography, CardMedia, Box, Button, Grid, CircularProgress, Container, Link } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { submitBook } from '@/actions/submit-book.action';
 import { BookCreationOptions } from '@/types/book';
@@ -71,16 +71,40 @@ export function BookSummary({bookCreationOptions} : {bookCreationOptions: BookCr
         
     }, [bookCreationOptions]);
 
+    function composeCoverComponent() {
+        if (coverImageUrl) return <CardMedia component="img" image={ coverImageUrl } className="object-cover" alt="Book cover" />
+        if (coverImageError) return <Typography>Failed to generate cover</Typography>
+        return (
+            <>
+                <CircularProgress />
+                <Typography className="text-xs">Generating cover...</Typography>
+            </>
+        )
+    }
+
     return (
-        <Box sx={{ textAlign: 'center', mt: 10, maxWidth: 600, mx: 'auto' }}>
-            <Typography>{text || "Creating your wonderful book..."}</Typography>
-            {creatingTextComplete && <Card sx={{ mt: 4, mb: 4, height: 512, width: 512, mx: 'auto' }}>
-                {coverImageError && <Typography>Failed to obtain image</Typography>}
-                {coverImageUrl && <CardMedia component="img" image={coverImageUrl} sx={{height: 512, width: 512}} alt="Book cover" />}
-                {!coverImageUrl && !coverImageError && <Typography>Let me create a cover now...</Typography>}
-            </Card>     
-            }
-            {creatingTextComplete && coverImageUrl && <Button variant="contained" sx={{ mt: 3 }} onClick={() => submitBook(bookCreationOptions.idea, text, coverImageUrl)}>Preview Final Version</Button>}
+        <Box sx={{ mt: 10, maxWidth: 1280, mx: 'auto' }} className="rounded-lg shadow-lg">
+            <Grid container spacing={2} padding={2}>
+                <Grid size={4}>
+                    <Container className="aspect-square rounded-lg overflow-hidden">
+                        { composeCoverComponent() }
+                    </Container>                    
+                </Grid>
+                <Grid size={8}>
+                    <Typography fontWeight={600} fontSize={24} color='#111927'>Story Summary</Typography>
+                    <Typography mb={10} fontWeight={500} fontSize={16} lineHeight={2} color='#111927'>{text || "Creating your wonderful book..."}</Typography>
+                    <Typography>Best for ages 0 to 8</Typography>
+                    <Typography>Ready in under 60 seconds</Typography>
+                    <Typography>Just $9.99 with 7-day ‘love it’ guarantee</Typography>
+                    { creatingTextComplete && coverImageUrl &&
+                    <>                    
+                    <Button variant="contained" fullWidth sx={{ mt: 3 }} onClick={() => submitBook(bookCreationOptions.idea, text, coverImageUrl)}>Get My Book Now – $9.99</Button>
+                    <Typography>(Safe, secure checkout in seconds)</Typography>
+                    <Link href="/">Back to the story creation page</Link>
+                    </>
+                    }
+                </Grid>
+            </Grid>
         </Box>
     )
 }
