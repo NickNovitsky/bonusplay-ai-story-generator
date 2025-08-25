a25701fb1965f906d8feb1c9dddfa4e4c5a12ac4
Nick Novitsky <nnovitsky@gamesoft.com>
2025-08-25 21:01:43 +0300

    Feat: Update site header

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,22 +1,21 @@
 import Image from 'next/image';
-import { Container, Grid, Link, Typography } from "@mui/material";
+import { Container, Grid, Link } from "@mui/material";
+import NextLink from 'next/link';
 
 function SiteHeader() {
     return (
     
-    <Container sx={{}}>
+    <Container>
         <Grid container py={8}>
             <Grid>
-                <Link href="/" sx={{ color: "#111927"}}>
-                    <Grid container alignItems="center">
-                        <Image src="/bonusplay-logo.svg" alt="BonusPlay Logo" width={34} height={34} />
-                        <Typography>BonusPlay</Typography>
-                    </Grid>
+                <Link href="/">
+                    <Image src="/bonusplay-logo-full.svg" width={166} height={30} alt="BonusPlay Logo" />
                 </Link>
             </Grid>
             <Grid size="grow" sx={{ textAlign: "right" }}>
-                <Link href="/support">Support</Link>
-                <Link href="/affiliate">Affiliate</Link>
+                <Link component={NextLink} variant='body2' ml={1} href="/about" sx={{ textDecoration: 'none', color: '#111927' }}>About</Link>
+                <Link component={NextLink} variant='body2' ml={1} href="/support" sx={{ textDecoration: 'none', color: '#111927' }}>Support</Link>
+                <Link component={NextLink} variant='body2' ml={1} href="/affiliate" sx={{ textDecoration: 'none', color: '#111927' }}>Affiliate</Link>
             </Grid>
         </Grid>
     </Container>
