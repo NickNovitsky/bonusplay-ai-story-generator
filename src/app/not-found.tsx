1a9694a50782802bdee495a2155e51b5984765a2
Nick Novitsky <nnovitsky@gamesoft.com>
2025-08-25 15:58:34 +0300

    Feat: Add 'not found' page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import { Container, Typography } from "@mui/material";
+
+export default function NotFound() {
+    return <Container><Typography textAlign={"center"}>Page not found</Typography></Container>
+}
